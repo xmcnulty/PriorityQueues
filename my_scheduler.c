d9e534386d13b3492e1a923a6b7a09e6708fb10d
Xavier D McNulty <xqe4@agate.cs.unh.edu>
2016-05-06 13:50:03 -0400

    implemented files

--- my_scheduler.c
+++ my_scheduler.c
@@ -8,7 +8,17 @@ void my_move_threads_from_newq_to_readyq(my_pqueue_t *nq, my_pqueue_t *rq) {
     while (cur) {
         my_pqueue_delete(nq, cur);
 
-        if (favornew)
+		int priority;
+		
+        if (favornew) {
+			priority = my_pqueue_favorite_prio(rq);
+		} else {
+			priority = MY_PRIO_STD;
+		}
+		
+		my_pqueue_insert(rq, priority, cur);
+		
+		cur = my_pqueue_walk(nq, cur, MY_WALK_PREV);
     }
 }
 
@@ -20,10 +30,19 @@ my_tcb_t my_find_next_thread_to_schedule(my_pqueue_t *rq) {
 void my_dispatcher(my_xstate_t* schedstate, my_xstate_t* currentstate){
 }
 
+/* Handles a dead thread. If the thread is not joinable call my_tcb_free,
+	otherwise insert the thread to the dead threads queue with MY_PRIO_STD */
 void my_handle_dead_thread(my_tcb_t current, my_pqueue_t *dq) {
+	if (!current->joinable) {
+		my_tcb_free(current);
+	} else {
+		my_queue_insert(dq, MY_PRIO_STD, current);
+	}
 }
 
+/* Just insert the current thread into the waiting queue (WQ) preserving its current priority */
 void my_handle_waiting_thread(my_tcb_t current, my_pqueue_t *wq) {
+		my_pqueue_insert(wq, current->q_prio, current);
 }
 
 void my_refresh_readyq(my_tcb_t current, my_pqueue_t *rq) {
